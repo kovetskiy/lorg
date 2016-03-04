1279a7c42ad2e140a63d1c36c1fa339580d2751e
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-03-04 18:26:28 +0600

    tests: fake stack for placeholders

--- placeholders_test.go
+++ placeholders_test.go
@@ -20,10 +20,14 @@ func TestPlaceholderLevel_ReturnsLevelStringRepresentation(t *testing.T) {
 func TestPlaceholderLine_ReturnsCallerLine(t *testing.T) {
 	_, _, line, _ := runtime.Caller(0)
 	assert.Equal( // +1
-		t, strconv.Itoa(line+2), PlaceholderLine(LevelDebug, ""), // +2
-	) // +3
-	assert.Equal( // +4
-		t, strconv.Itoa(line+5), PlaceholderLine(LevelWarning, "blah"), // +5
+		t, // +2
+		strconv.Itoa(line+4),                                             // +3
+		callPlaceholderAtDeepStackLevel(PlaceholderLine, LevelDebug, ""), // +4
+	) // +5
+	assert.Equal( // +6
+		t, // +7
+		strconv.Itoa(line+9),                                               // +8
+		callPlaceholderAtDeepStackLevel(PlaceholderLine, LevelWarning, ""), // +9
 	)
 }
 
@@ -33,7 +37,9 @@ func TestPlaceholderFile_ReturnsCallerFilenameInShortModeByDefault(
 	_, file, _, _ := runtime.Caller(0)
 
 	assert.Equal(
-		t, filepath.Base(file), PlaceholderFile(LevelInfo, ""),
+		t,
+		filepath.Base(file),
+		callPlaceholderAtDeepStackLevel(PlaceholderFile, LevelInfo, ""),
 	)
 }
 
@@ -43,7 +49,9 @@ func TestPlaceholderFile_ReturnsCallerFilenameInShortModeIfValueIsShort(
 	_, file, _, _ := runtime.Caller(0)
 
 	assert.Equal(
-		t, filepath.Base(file), PlaceholderFile(LevelDebug, "short"),
+		t,
+		filepath.Base(file),
+		callPlaceholderAtDeepStackLevel(PlaceholderFile, LevelDebug, "short"),
 	)
 }
 
@@ -53,10 +61,14 @@ func TestPlaceholderFile_ReturnsCallerFullFilenameInLongMode(
 	_, file, _, _ := runtime.Caller(0)
 
 	assert.Equal(
-		t, file, PlaceholderFile(LevelDebug, "long"),
+		t,
+		file,
+		callPlaceholderAtDeepStackLevel(PlaceholderFile, LevelDebug, "long"),
 	)
 	assert.Equal(
-		t, file, PlaceholderFile(LevelWarning, "long"),
+		t,
+		file,
+		callPlaceholderAtDeepStackLevel(PlaceholderFile, LevelWarning, "long"),
 	)
 }
 
@@ -83,5 +95,46 @@ func TestPlaceholderTime_ReturnsTimeUsingSpecifiedLayout(t *testing.T) {
 		t,
 		time.Now().Format(time.Kitchen),
 		PlaceholderTime(LevelDebug, time.Kitchen),
+	)
+}
+
+// Placeholders which uses runtime.Caller for receiving data about caller
+// should be tested using this helper because Placeholder function will be
+// executed by Formatter at N level of stack trace below than calling some log
+// function.
+//
+// For example:
+// 1 level: github.com/user/project: call Log.Warning
+// 2 level: lorg: Log.Warning call log.log
+// 3 level: lorg: Log.log call Log.doLog
+// 4 level: lorg: log.doLog call Format.Render
+// 5 level: lorg: Format.Render call Placeholder
+//
+// Actually, this function is 2nd level of stack trace to call placeholder
+// 1st level of stack trace is testcase
+func callPlaceholderAtDeepStackLevel(
+	placeholder Placeholder, logLevel Level, placeholderValue string,
+) string {
+	actualStackLevel := 2
+
+	return doRecursiveCallPlaceholder(
+		placeholder, logLevel, placeholderValue,
+		actualStackLevel, PlaceholderCallLevel,
+	)
+}
+
+func doRecursiveCallPlaceholder(
+	placeholder Placeholder, logLevel Level, placeholderValue string,
+	actualStackLevel, expectedStackLevel int,
+) string {
+	actualStackLevel++
+
+	if actualStackLevel >= expectedStackLevel {
+		return placeholder(logLevel, placeholderValue)
+	}
+
+	return doRecursiveCallPlaceholder(
+		placeholder, logLevel, placeholderValue,
+		actualStackLevel, expectedStackLevel,
 	)
 }
